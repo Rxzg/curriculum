549130094caa9c7bb9e208ca0faf7052ec13856b
shaw <11111@qq.com>
2021-02-10 01:41:09 +0800

    [MOD] 暂时写死

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import {genMongoUrl} from "./utils/db";
 import {WechatModule} from "./modules/wechat/v1/wechat.module";
 
 // MongooseModule.forRoot(genMongoUrl(require('../../config/db/mongo').development))
+const mongoConfig = {
+    "host": "127.0.0.1",
+    "port": 27019,
+    "db": "op",
+    "user": "",
+    "password": ""
+}
 @Module({
     imports: [
-        MongooseModule.forRoot(genMongoUrl(require('../../config/db/mongo').development)),
+        MongooseModule.forRoot(mongoConfig as any),
         WechatModule],
     controllers: [],
     providers: [],
