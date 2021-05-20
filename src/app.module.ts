5861b9ac65c6e073e6370761520e34013fd20028
shaw <11111@qq.com>
2021-05-20 11:22:54 +0800

    [MOD] 数据库路径修改

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,10 @@ import {MongooseModule} from "@nestjs/mongoose";
 import {genMongoUrl} from "./utils/db";
 import {WechatModule} from "./modules/wechat/v1/wechat.module";
 
-// MongooseModule.forRoot(genMongoUrl(require('../../config/db/mongo').development))
-const mongoConfig = {
-    "host": "127.0.0.1",
-    "port": 27019,
-    "db": "op",
-    "user": "",
-    "password": ""
-}
+const mongoConfig = genMongoUrl(require('../config/db/mongo'));
 @Module({
     imports: [
-        MongooseModule.forRoot(genMongoUrl(mongoConfig)),
+        MongooseModule.forRoot(mongoConfig),
         WechatModule],
     controllers: [],
     providers: [],
